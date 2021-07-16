a740331901b04687e2135343d226c26c5e8be1d2
codingwithin <86608995+codingwithin@users.noreply.github.com>
2021-07-16 23:18:48 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,34 +1,87 @@
+var saveGame = localStorage.getItem('polSimSave')
 var gameData = {
-    gold: 0,
-    goldPerClick: 1,
-    goldPerClickCost: 10
+  dollarsStolen: 0,
+  ballpenCost: 0.5,
+  ballpenPrice: 1,
+  ballpenDemand: 1,
+  ballpenPenaltyRef: 1.5,
+  lastTick: Date.now()
+}
 
+function update(id, content) {
+  document.getElementById(id).innerHTML = content;
 }
 
 function mineGold() {
-    gameData.gold += gameData.goldPerClick
-    document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
-  }
+  gameData.gold += gameData.goldPerClick
+  update("goldMined", format(gameData.gold) + " Gold Mined")
+}
 
-  function buyGoldPerClick() {
-    if (gameData.gold >= gameData.goldPerClickCost) {
-      gameData.gold -= gameData.goldPerClickCost
-      gameData.goldPerClick += 1
-      gameData.goldPerClickCost *= 2
-      document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
-      document.getElementById("perClickUpgrade").innerHTML = "Upgrade Pickaxe (Currently Level " + gameData.goldPerClick + ") Cost: " + gameData.goldPerClickCost + " Gold"
-    }
+function buyGoldPerClick() {
+  if (gameData.gold >= gameData.goldPerClickCost) {
+    gameData.gold -= gameData.goldPerClickCost
+    gameData.goldPerClick += 1
+    gameData.goldPerClickCost *= 2
+    update("goldMined", format(gameData.gold) + " Gold Mined")
+    update("perClickUpgrade", "Upgrade Pickaxe (Currently Level " + format(gameData.goldPerClick, "scientific") + ") Cost: " + format(gameData.goldPerClickCost, "scientific") + " Gold")
   }
+}
+
+function incPriceBallpen() {
+  gameData.ballpenPrice += 0.01
+  update("ballpenPrice", "Ballpen price: $" + format(gameData.ballpenPrice,"",2))
+  ballpenProbabilityPenalty = Math.min(1,Math.exp(gameData.ballpenPrice)/Math.exp(gameData.ballpenPenaltyRef))
+  update("ballpenPenaltyProbability","Penalty probability: "+format(ballpenProbabilityPenalty*100,"",2)+"%")
+  ballpenPenalty = (gameData.ballpenPrice - gameData.ballpenCost)*((gameData.ballpenPrice/gameData.ballpenPenaltyRef)**(1.5))
+  update("ballpenPenalty","Penalty if caught: $" + format(ballpenPenalty,"",2))
+}
+
+function decPriceBallpen() {
+  gameData.ballpenPrice -= 0.01
+  update("ballpenPrice", "Ballpen price: $" + format(gameData.ballpenPrice,"",2))
+  ballpenProbabilityPenalty = Math.min(1,Math.exp(gameData.ballpenPrice)/Math.exp(gameData.ballpenPenaltyRef))
+  update("ballpenPenaltyProbability","Penalty probability: "+format(ballpenProbabilityPenalty*100,"",2)+"%")
+  ballpenPenalty = (gameData.ballpenPrice - gameData.ballpenCost)*((gameData.ballpenPrice/gameData.ballpenPenaltyRef)**(1.5))
+  update("ballpenPenalty","Penalty if caught: $" + format(ballpenPenalty,"",2))
+}
 
 var mainGameLoop = window.setInterval(function() {
-  mineGold()
+  diff = Date.now() - gameData.lastTick;
+  update("ballpenPenaltyNotification","")
+  gameData.lastTick = Date.now()
+  gameData.dollarsStolen += (gameData.ballpenPrice - gameData.ballpenCost)*diff*gameData.ballpenDemand/1000
+  ballpenProbabilityPenalty = Math.min(1,Math.exp(gameData.ballpenPrice)/Math.exp(gameData.ballpenPenaltyRef))
+  ballpenProbability = Math.random()
+  if (ballpenProbability < ballpenProbabilityPenalty) {
+    gameData.dollarsStolen -= (gameData.ballpenPrice - gameData.ballpenCost)*diff*gameData.ballpenDemand*((gameData.ballpenPrice/gameData.ballpenPenaltyRef)**(1.5))/1000
+    update("ballpenPenaltyNotification","Incurred Penalty!")
+  } 
+  update("dollarsStolen", "$"+format(gameData.dollarsStolen,"",2)+" Stolen")
 }, 1000)
 
 var saveGameLoop = window.setInterval(function() {
-  localStorage.setItem("goldMinerSave", JSON.stringify(gameData))
+  localStorage.setItem('polSimSave', JSON.stringify(gameData))
 }, 15000)
 
-var savegame = JSON.parse(localStorage.getItem("goldMinerSave"))
-if (savegame !== null) {
-  gameData = savegame
+function format(number, type, dec) {
+	let exponent = Math.floor(Math.log10(number))
+	let mantissa = number / Math.pow(10, exponent)
+	if (exponent < 3) return number.toFixed(dec)
+	if (type == "scientific") return mantissa.toFixed(2) + "e" + exponent
+	if (type == "engineering") return (Math.pow(10, exponent % 3) * mantissa).toFixed(2) + "e" + (Math.floor(exponent / 3) * 3)
 }
+
+
+if (typeof saveGame.gold !== "undefined") gameData.gold = saveGame.gold;
+if (typeof saveGame.goldPerClick !== "undefined") gameData.goldPerClick = saveGame.goldPerClick;
+if (typeof saveGame.goldPerClickCost !== "undefined") gameData.goldPerClickCost = saveGame.goldPerClickCost;
+if (typeof saveGame.lastTick !== "undefined") gameData.lastTick = saveGame.lastTick;
+
+function tab(tab) {
+  // hide all your tabs, then show the one the user selected.
+  document.getElementById("mineGoldMenu").style.display = "none"
+  document.getElementById("shopMenu").style.display = "none"
+  document.getElementById(tab).style.display = "inline-block"
+}
+// go to a tab for the first time, so not all show
+tab("mineGoldMenu")
